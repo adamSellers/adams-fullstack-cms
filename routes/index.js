336c3cd1063b035dfe868a184c679fdf177061af
Adam Sellers <adza007@gmail.com>
2020-08-28 17:03:09 +1000

    updating react build script

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 const authRoutes = require('./authRoutes');
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 /* Salesforce authroutes */
 authRoutes.salesforceAuthRoutes(router);
 
