3613012bfef864ef7bf317bc0684e80e238f0631
Adam Sellers <adza007@gmail.com>
2020-08-31 13:35:22 +1000

    routing implemented, now with added buttons!

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -49,7 +49,7 @@ class Navbar extends Component {
   showLogout() {
     if (this.props.auth) {
       return (
-        <Menu.Item name='logout' position="right" as={Link} to='/auth/logout'>
+        <Menu.Item name='logout' position="right" as='a' href='/auth/logout'>
             <Button primary animated="vertical">
               <Button.Content visible>Logout</Button.Content>
               <Button.Content hidden>
