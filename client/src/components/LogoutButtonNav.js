28455526ef6a25fe513f9faa2e0a6e0a1aa1fa5a
Adam Sellers <adza007@gmail.com>
2020-08-31 18:50:10 +1000

    funking up the profile component

--- client/src/components/LogoutButtonNav.js
+++ client/src/components/LogoutButtonNav.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Button, Icon, Menu } from 'semantic-ui-react';
 
 const LogoutButtonNav = () => {
-          <Button primary animated="vertical">
-            <Button.Content visible>Logout</Button.Content>
-            <Button.Content hidden>
-              <Icon name="sign-out" />
-            </Button.Content>
-          </Button>
+  return (
+    <Button primary animated="vertical">
+      <Button.Content visible>Logout</Button.Content>
+      <Button.Content hidden>
+        <Icon name="sign-out" />
+      </Button.Content>
+    </Button>
+  );
 };
 
 export default LogoutButtonNav;
