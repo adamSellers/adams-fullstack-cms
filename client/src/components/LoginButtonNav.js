99db780e551f17c27451937e2167c7256829fdf4
Adam Sellers <adza007@gmail.com>
2020-08-31 13:25:51 +1000

    about to break it, pretty sure

--- client/src/components/LoginButtonNav.js
+++ client/src/components/LoginButtonNav.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
 import { Button, Icon } from 'semantic-ui-react';
 
 export default class LoginButtonNav extends Component {
   render() {
     return (
-      <Button primary animated="vertical" as='a' path='/auth/login'>
+      <Button primary animated="vertical" as='a' to='/auth/login'>
         <Button.Content visible>Login</Button.Content>
         <Button.Content hidden>
           <Icon name="cloud" />
