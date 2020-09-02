e47ca20369322cc060bc75e82ffbb34b32c298f5
Adam Sellers <adza007@gmail.com>
2020-09-03 08:46:18 +1000

    csp sucks ass

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
 import * as actions from '../actions';
 
 // components imported
@@ -21,6 +22,12 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <Helmet>
+          <title>CMS Helmet</title>
+          <meta http-equiv="Content Security Policy" content="default-src *;
+   img-src * 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *;
+   style-src  'self' 'unsafe-inline' *"></meta>
+        </Helmet>
         <Container>
           <Navbar />
           <Route exact path="/">
