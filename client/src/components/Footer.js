badd196438fef565487015a0580d9c7dd5dc65b7
Adam Sellers <adza007@gmail.com>
2020-09-03 21:06:10 +1000

    wow, that really broke it

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
           <Grid.Column width="4">
             <Image src="/footerLogo.png" alt="Salesforce.org Architects"></Image>
           </Grid.Column>
-          <Grid.Column width="8">
+          <Grid.Column width="8" textAlign="center">
             <Menu inverted vertical>
               <Menu.Item>
                 <a href="https://www.salesforce.org/nonprofit/nonprofit-success-pack/">NPSP</a>
