ba1d4589d3489a1e5fa4e0ba0082d288631f8526
Adam Sellers <adza007@gmail.com>
2020-09-06 08:24:55 +1000

    adding get communities reducer - so many bugz

--- client/src/reducers/communitiesReducer.js
+++ client/src/reducers/communitiesReducer.js
@@ -2,6 +2,8 @@ import { GET_COMMUNITIES } from '../actions/types';
 
 // create the communities reducer and export it
 export default function(state = null, action) {
+  console.log(`reducer action: ${JSON.stringify(action)}`);
+  console.log(`reducer state: ${JSON.stringify(state)}`);
   switch (action.type) {
     case GET_COMMUNITIES:
       return action.payload || false;
