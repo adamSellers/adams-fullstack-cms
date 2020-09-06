739c34601eb7264733055c9c39fbc0fe962b0c6c
Adam Sellers <adza007@gmail.com>
2020-09-06 15:08:51 +1000

    adding get communities reducer - so many bugz

--- client/src/components/CommunityDropdown.js
+++ client/src/components/CommunityDropdown.js
@@ -6,10 +6,22 @@ import { Dropdown, Icon } from 'semantic-ui-react';
 class CommunityDropdown extends Component {
   componentDidUpdate() {
     console.log(`are there props?: ${JSON.stringify(this.props.communities)}`);
+    const options = this.props.communities.map(community => {
+      return {
+        key: community.id,
+        text: community.name,
+        value: community.id
+      }
+    });
   }
   render() {
     return(
-      <Dropdown placeholder="select a community" fluid />
+      <Dropdown 
+        placeholder="select a community" 
+        fluid
+        options={this.options}
+        onChange={() => console.log(`on change selected, value is: ${options.value}`)}
+        />
     );
   };
 }
