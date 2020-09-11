178ea1641e189f8b8f518e6f2fea3d4046bec8d0
Adam Sellers <adza007@gmail.com>
2020-09-12 05:52:44 +1000

    async redux - this will be hard

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 export const FETCH_USER = 'fetch_user';
-export const GET_COMMUNITIES = 'get_communities';
+export const GET_COMMUNITIES_SUCCESS = 'get_communities_success';
+export const GET_COMMUNITIES_STARTED = 'get_communities_started';
+export const GET_COMMUNITIES_FAILURE = 'get_communities_failure';
 
 // this action type takes in a paramater
 export const GET_CONTENT_LIST = 'get_content_list';
