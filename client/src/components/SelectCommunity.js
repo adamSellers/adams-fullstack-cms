9b6958f16ef59a1a3ec739485a02cb3ed3d11a70
Adam Sellers <adza007@gmail.com>
2020-09-12 15:01:09 +1000

    syntax bugz

--- client/src/components/SelectCommunity.js
+++ client/src/components/SelectCommunity.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Dropdown } from 'semantic-ui-react';
+import { fetchCommunities } from '../actions';
 
 class SelectCommunity extends Component {
 
