fffdb944f952e3282517d549e63e61baadf0e4a4
Adam Sellers <adza007@gmail.com>
2020-09-13 09:15:02 +1000

    home link navbar bug

--- client/src/reducers/communitiesReducer.js
+++ client/src/reducers/communitiesReducer.js
@@ -1,4 +1,4 @@
-import { communities } from '../actions/types';
+import { FETCH_COMMUNITIES } from '../actions/types';
 
 // set the initial state first
 const initialState = {
@@ -10,9 +10,7 @@ const initialState = {
 // set the state depending on the dispatch coming in
 const communtiesReducer = (state = initialState, action) => {
   switch(action.type) {
-    case "COMMUNITIES":
-      return {...state, isLoading: true};
-    case "communities_returned":
+    case FETCH_COMMUNITIES:
       // reduce the returned state down to an array of options
       // filter here to limit to active only
       const activeCommunities = [];
