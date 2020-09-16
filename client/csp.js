7973601dd661f67059faf1a40b64113d41c6c6e3
Adam Sellers <adza007@gmail.com>
2020-09-17 06:13:34 +1000

    hey this might be getting closer finally

--- client/csp.js
+++ client/csp.js
@@ -1,9 +1,19 @@
 module.exports = {
   prod: {
-    "default-src": "'self'",
+    "default-src": [
+      "'self'",
+      "https://*.salesforce.com"],
     "img-src": [
       "'self'",
       "https:"
+    ],
+    "font-src": [
+      "'self'",
+      "data:"
+    ],
+    "style-src": [
+      "'self'",
+      "data:"
     ]
   }
 }
