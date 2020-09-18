9237de69ca2e77776fc9cd3c2d9374f14cfac915
Adam Sellers <adza007@gmail.com>
2020-09-19 09:55:14 +1000

    turns out you should only try to change one thing at a time..

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Segment, Container, Card, Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class Profile extends Component {
   componentDidMount() {
