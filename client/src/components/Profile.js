7d83f4cf738506f43f3512d3c138532826ac8a9d
Adam Sellers <adza007@gmail.com>
2020-10-29 18:10:34 +1100

    trying to fix the profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -19,14 +19,13 @@ class Profile extends Component {
                   <Image src={this.props.auth.sfProfilePic} wrapped ui={false} />
                   <Card.Content>
                     <Card.Header>{`${this.props.auth.firstName} ${this.props.auth.lastName}`}</Card.Header>
-                    <Card.Description>{`Salesforce Auth Token: ${this.props.auth.sfAccessToken}`}</Card.Description>
                   </Card.Content>
                 </Card>
               </Container>
             </Grid.Column>
             <Grid.Column width={8} verticalAlign="middle">
-              SF User Id: {this.props.auth.userId} <br />
-              SF Instance URL: {this.props.auth.sfInstanceUrl}
+              <strong>SF User Id:</strong> {this.props.auth.userId} <br />
+              <strong>SF Instance URL:</strong> {this.props.auth.sfInstanceUrl}
             </Grid.Column>
           </Grid.Row>
         </Grid>
