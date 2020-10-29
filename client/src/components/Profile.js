d34e608359d12b6056f90740dc71d1f70266fa61
Adam Sellers <adza007@gmail.com>
2020-10-29 18:49:56 +1100

    trying to fix the profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -25,16 +25,16 @@ class Profile extends Component {
             <Grid.Column width={8} textAlign="center">
               <Container>
                 <Card>
-                  <Image src={this.props.auth.sfProfilePic} wrapped ui={false} />
+                  <Image src={this.props.auth.data.sfProfilePic} wrapped ui={false} />
                   <Card.Content>
-                    <Card.Header>{`${this.props.auth.firstName} ${this.props.auth.lastName}`}</Card.Header>
+                    <Card.Header>{`${this.props.auth.data.firstName} ${this.props.auth.data.lastName}`}</Card.Header>
                   </Card.Content>
                 </Card>
               </Container>
             </Grid.Column>
             <Grid.Column width={8} verticalAlign="middle">
-              <strong>SF User Id:</strong> {this.props.auth.userId} <br />
-              <strong>SF Instance URL:</strong> {this.props.auth.sfInstanceUrl}
+              <strong>SF User Id:</strong> {this.props.auth.data.userId} <br />
+              <strong>SF Instance URL:</strong> {this.props.auth.data.sfInstanceUrl}
             </Grid.Column>
           </Grid.Row>
         </Grid>
