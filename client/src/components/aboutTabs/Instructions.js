141e86d2e30b957dc2b4c627804678bb1d5bd22f
Adam Sellers <adza007@gmail.com>
2020-11-20 17:50:35 +1100

    building out the static content

--- client/src/components/aboutTabs/Instructions.js
+++ client/src/components/aboutTabs/Instructions.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Segment, Container } from 'semantic-ui-react';
+import { Segment, Container, Header, Icon } from 'semantic-ui-react';
 
 export default class Instructions extends Component {
   render() {
     return (
       <Segment placeholder>
         <Container text verticalAlign="middle" stretched>
-          Details on how to use this tool go here.
+        <h2 class="ui header"><i aria-hidden="true" class="plug icon"></i><div class="content">How to Use This Tool</div></h2>
         </Container>
       </Segment>
     )
