80127468977f6b711af50eda75f353bdc1542700
Adam Sellers <adza007@gmail.com>
2020-11-22 17:06:06 +1100

    making things clickable

--- client/src/components/DisplayContent.js
+++ client/src/components/DisplayContent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Segment, Grid, Card, Message, Image } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
--- client/src/components/SingleContent.js
+++ client/src/components/SingleContent.js
@@ -8,17 +8,12 @@ class SingleContent extends Component {
   // on mount, I want to filter out a single content item from state
   componentDidMount(){
     let contentId = this.props.match.params.id;
-    const singleItem = this.props.content.data.filter( item => {
-      // filter out for content ID
-      item.managedContentId == contentId;
-    });
+    const singleItem = this.props.content.data.filter( item => item.managedContentId == contentId);
     console.log(`in single item cmp - single item array is: ${JSON.stringify(singleItem)}`);
   };
 
   // then i want to render a single full column width card with the article details from state
   render() {
-    // want to grab the parameter passed in and find the single item from managed content id
-    let contentId = localStorage.getItem("managedContentId");
     return (
       <Segment>
         <Container>
@@ -27,7 +22,6 @@ class SingleContent extends Component {
       </Segment>
     )
   };
-
 }
 
 const mapStateToProps = ({ content }) => {
