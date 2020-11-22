d47109cc085e0390de44c8c38ad387f337a6cf17
Adam Sellers <adza007@gmail.com>
2020-11-22 17:21:47 +1100

    making things clickable

--- client/src/components/DisplayContent.js
+++ client/src/components/DisplayContent.js
@@ -19,7 +19,7 @@ class DisplayContent extends Component {
       const cards = this.props.content.data.map( article => {
         return (
           <Grid.Column stretched>
-            <Card key={article.key} onClick={this.handleOnClick}>
+            <Card key={article.key} onClick={(event, data) => this.handleOnClick(event, data)}>
               <Image src={this.props.auth.data.sfInstanceUrl + article.image} />
               <Card.Content>
                 <Card.Header>
