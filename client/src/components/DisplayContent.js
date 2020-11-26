332049eeb818fd16da0ac7e4da51b074cd7b53f1
Adam Sellers <adza007@gmail.com>
2020-11-27 08:50:26 +1100

    the clickable component again

--- client/src/components/DisplayContent.js
+++ client/src/components/DisplayContent.js
@@ -40,8 +40,7 @@ class DisplayContent extends Component {
 
   handleOnClick( event, data ) {
     // function to navigate to single article
-    console.log(`event is: ${JSON.stringify(event)}`);
-    console.log(`data is: ${JSON.stringify(data)}`);
+    console.log(`data is: ${JSON.stringify(data['value'])}`);
   }
 
   render() {
