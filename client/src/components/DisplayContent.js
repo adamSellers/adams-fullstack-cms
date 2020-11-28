a652b5c71d0cde3aeab1d9eaf60c152af8f5bdb3
Adam Sellers <adza007@gmail.com>
2020-11-29 07:23:41 +1100

    the clickable component again

--- client/src/components/DisplayContent.js
+++ client/src/components/DisplayContent.js
@@ -20,7 +20,7 @@ class DisplayContent extends Component {
         let contentId = article.key;
         return (
           <Grid.Column stretched>
-            <Card key={contentId} onClick={ ( event, data) => this.handleOnClick(event, data)} style={{ marginBottom: '2em'}}>
+            <Card key={contentId} onClick={ ( event, contentId) => this.handleOnClick(event, contentId)} style={{ marginBottom: '2em'}}>
               <Image src={this.props.auth.data.sfInstanceUrl + article.image} />
               <Card.Content>
                 <Card.Header>
@@ -38,9 +38,9 @@ class DisplayContent extends Component {
     }
   }
 
-  handleOnClick( event, data ) {
+  handleOnClick( event, contentId ) {
     // function to navigate to single article
-    console.log(`data is: ${JSON.stringify(data['value'])}`);
+    console.log(`data is: ${contentId}`);
   }
 
   render() {
