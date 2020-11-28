2159cea2c559a389ff31f811fa7e06fb2097a3ea
Adam Sellers <adza007@gmail.com>
2020-11-29 08:19:36 +1100

    just trying to make it clickable

--- client/src/components/DisplayContent.js
+++ client/src/components/DisplayContent.js
@@ -40,7 +40,7 @@ class DisplayContent extends Component {
 
   handleOnClick( event, data ) {
     // function to navigate to single article
-    console.log(`data is: ${JSON.stringify(data)}`);
+    console.log(`data is: ${JSON.stringify(data['value'])}`);
   }
 
   render() {
