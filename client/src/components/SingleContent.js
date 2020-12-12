8bdde3e9c4f7192207731821bc355e76b95c7a4e
Adam Sellers <adza007@gmail.com>
2020-12-12 13:17:31 +1100

    make it clickable

--- client/src/components/SingleContent.js
+++ client/src/components/SingleContent.js
@@ -4,14 +4,11 @@ import { Segment, Container, Card } from 'semantic-ui-react';
 
 
 class SingleContent extends Component {
-  constructor(props) {
-    super(props);
-    const contentId = this.props.match.params.id;
-  }
 
   // on mount, I want to filter out a single content item from state
   componentDidMount(){
-    const singleItem = this.props.content.data.filter( item => item.managedContentId === this.contentId);
+    const contentId = this.props.match.params.id;
+    const singleItem = this.props.content.data.filter( item => item.managedContentId === contentId);
     console.log(`in single item cmp - single item array is: ${JSON.stringify(singleItem)}`);
   };
 
