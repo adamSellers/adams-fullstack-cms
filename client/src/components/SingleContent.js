2969f0ce5be8ff24fe657d3a8b7c4b3111a47c0a
Adam Sellers <adza007@gmail.com>
2020-12-22 14:31:13 +1100

    adding single content component styling

--- client/src/components/SingleContent.js
+++ client/src/components/SingleContent.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Card } from 'semantic-ui-react';
 import { Segment, Container, Card } from 'semantic-ui-react';
 
 
 class SingleContent extends Component {
 
   state = {
-    contentId: this.props.match.params.contentId
+    contentId: this.props.match.params.contentId,
+    singleItem: []
   };
 
   // on mount, I want to filter out a single content item from state
   componentDidMount(){
-    console.log(`the params stuff: ${JSON.stringify(this.props.match.params)}`);
-    console.log(`the contentId is: ${this.state.contentId}`);
-    console.log(`the params match is: ${this.props.match.params.id}`);
-    const singleItem = this.props.content.data.filter( item => item.key === this.state.contentId);
-    console.log(`in single item cmp - single item array is: ${JSON.stringify(singleItem)}`);
+    this.state.singleItem = this.props.content.data.filter( item => item.key === this.state.contentId);
   };
 
   // then i want to render a single full column width card with the article details from state
@@ -23,7 +21,13 @@ class SingleContent extends Component {
     return (
       <Segment>
         <Container>
-          Detail for managed Content ID goes here. 
+          <Card 
+            centered
+            fluid
+            header={this.state.singleItem[0].title}
+            description={this.state.singleItem[0].body}
+            extra={`${this.state.singleItem[0].typeLable} Content Type - powered by Salesforce`}
+          />
         </Container>
       </Segment>
     )
