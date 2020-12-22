19b1fd8e1780a26484798d865bfb953467bec6a0
Adam Sellers <adza007@gmail.com>
2020-12-22 14:34:14 +1100

    adding single content component styling

--- client/src/components/SingleContent.js
+++ client/src/components/SingleContent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card } from 'semantic-ui-react';
 import { Segment, Container, Card } from 'semantic-ui-react';
 
 
